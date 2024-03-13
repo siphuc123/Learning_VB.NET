940e85b8ccb5b1d86fcb93f9c50a7e06bb21a144
PhucNST.Dev <phucnst2104@gmail.com>
2024-03-13 13:08:22 +0700

    13/04/2024
    + Learning about MsgBox, MessageBox and Variables

---
+++ BasicBtn/LearningVB/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub btnMsg_Click(sender As Object, e As EventArgs) Handles btnMsg.Click
+
+        MsgBox("Hello Word")
+
+        MessageBox.Show("Checkin, Xin chào", "Lời chào đầu", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+    End Sub
+End Class
---
+++ BasicBtn/LearningVB/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("LearningVB")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("LearningVB")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d9ab6bf6-1c8b-4fed-9d99-80c220e1dd6f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
